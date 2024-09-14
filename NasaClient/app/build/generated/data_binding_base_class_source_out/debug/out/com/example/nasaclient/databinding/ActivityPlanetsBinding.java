3a86f675cd52a0245c52249799cc05c8c6fc44f2
vuxy98 <clusterbomb74@gmail.com>
2024-09-14 15:12:16 +0700

    Some update for the planets tab

--- NasaClient/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/nasaclient/databinding/ActivityPlanetsBinding.java
+++ NasaClient/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/nasaclient/databinding/ActivityPlanetsBinding.java
@@ -4,25 +4,43 @@ package com.example.nasaclient.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.cardview.widget.CardView;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.nasaclient.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityPlanetsBinding implements ViewBinding {
   @NonNull
-  private final ConstraintLayout rootView;
+  private final CardView rootView;
 
-  private ActivityPlanetsBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final Button exploreButton;
+
+  @NonNull
+  public final ImageView planetImage;
+
+  @NonNull
+  public final TextView titleText;
+
+  private ActivityPlanetsBinding(@NonNull CardView rootView, @NonNull Button exploreButton,
+      @NonNull ImageView planetImage, @NonNull TextView titleText) {
     this.rootView = rootView;
+    this.exploreButton = exploreButton;
+    this.planetImage = planetImage;
+    this.titleText = titleText;
   }
 
   @Override
   @NonNull
-  public ConstraintLayout getRoot() {
+  public CardView getRoot() {
     return rootView;
   }
 
@@ -43,10 +61,31 @@ public final class ActivityPlanetsBinding implements ViewBinding {
 
   @NonNull
   public static ActivityPlanetsBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.exploreButton;
+      Button exploreButton = ViewBindings.findChildViewById(rootView, id);
+      if (exploreButton == null) {
+        break missingId;
+      }
 
-    return new ActivityPlanetsBinding((ConstraintLayout) rootView);
+      id = R.id.planetImage;
+      ImageView planetImage = ViewBindings.findChildViewById(rootView, id);
+      if (planetImage == null) {
+        break missingId;
+      }
+
+      id = R.id.titleText;
+      TextView titleText = ViewBindings.findChildViewById(rootView, id);
+      if (titleText == null) {
+        break missingId;
+      }
+
+      return new ActivityPlanetsBinding((CardView) rootView, exploreButton, planetImage, titleText);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
